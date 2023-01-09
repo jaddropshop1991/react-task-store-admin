4570dbca8ea3355a5d849f114b21b2fc97d745cb
jaddropshop1991 <101974310+jaddropshop1991@users.noreply.github.com>
2023-01-09 04:47:38 +0200

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:500/api/";
+const BASE_URL = "https://react-task-store-backend2.onrender.com/api/";
 
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken ;
 // const TOKEN = () => {
